2699b7dc7bc08d515174ac442e232b132a61bef3
NewAchiever <kaizokugarinozoro98@gmail.com>
2024-09-28 12:10:56 -0400

    added v-for counter

--- front-end/src/components/ProductsList.vue
+++ front-end/src/components/ProductsList.vue
@@ -3,10 +3,11 @@
     <div
       class="product-item"
       v-if="products.length > 0"
-      v-for="product in products"
+      v-for="(product, index) in products"
       :key="product.id"
     >
       <img :src="product.imageUrl" />
+      <span>{{ index + 1 }}</span>
       <h3 class="product-name">{{ product.name }}</h3>
       <p class="product-price">{{ product.price }}</p>
       <router-link :to="'/products/' + product.id">
